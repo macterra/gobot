0c399a069e89fcc5bc5c4c6d96418f8dc0443cce
David McFadzean <davidmc@gmail.com>
2021-11-28 15:13:44 -0500

    Added responder

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/slack-go/slack"
@@ -60,7 +63,7 @@ func main() {
 					socketClient.Ack(*event.Request)
 					// Now we have an Events API event, but this event type can in turn be many types, so we actually need another type switch
 					//log.Println(eventsAPIEvent)
-					err := handleEventMessage(eventsAPIEvent)
+					err := handleEventMessage(eventsAPIEvent, client)
 					if err != nil {
 						// Replace with actual err handeling
 						log.Fatal(err)
@@ -74,7 +77,7 @@ func main() {
 }
 
 // handleEventMessage will take an event and handle it properly based on the type of event
-func handleEventMessage(event slackevents.EventsAPIEvent) error {
+func handleEventMessage(event slackevents.EventsAPIEvent, client *slack.Client) error {
 	switch event.Type {
 	// First we check if this is an CallbackEvent
 	case slackevents.CallbackEvent:
@@ -84,10 +87,56 @@ func handleEventMessage(event slackevents.EventsAPIEvent) error {
 		switch ev := innerEvent.Data.(type) {
 		case *slackevents.AppMentionEvent:
 			// The application has been mentioned since this Event is a Mention event
-			log.Println(ev)
+			err := handleAppMentionEvent(ev, client)
+			if err != nil {
+				return err
+			}
 		}
 	default:
 		return errors.New("unsupported event type")
+	}
+	return nil
+}
+
+// handleAppMentionEvent is used to take care of the AppMentionEvent when the bot is mentioned
+func handleAppMentionEvent(event *slackevents.AppMentionEvent, client *slack.Client) error {
+
+	// Grab the user name based on the ID of the one who mentioned the bot
+	user, err := client.GetUserInfo(event.User)
+	if err != nil {
+		return err
+	}
+	// Check if the user said Hello to the bot
+	text := strings.ToLower(event.Text)
+
+	// Create the attachment and assigned based on the message
+	attachment := slack.Attachment{}
+	// Add Some default context like user who mentioned the bot
+	attachment.Fields = []slack.AttachmentField{
+		{
+			Title: "Date",
+			Value: time.Now().String(),
+		}, {
+			Title: "Initializer",
+			Value: user.Name,
+		},
+	}
+	if strings.Contains(text, "hello") {
+		// Greet the user
+		attachment.Text = fmt.Sprintf("Hello %s", user.Name)
+		attachment.Pretext = "Greetings"
+		attachment.Color = "#4af030"
+	} else {
+		// Send a message to the user
+		attachment.Text = fmt.Sprintf("How can I help you %s?", user.Name)
+		attachment.Pretext = "How can I be of service"
+		attachment.Color = "#3d3d3d"
+	}
+	// Send the message to the channel
+	// The Channel is available in the event message
+	_, _, err = client.PostMessage(event.Channel, slack.MsgOptionAttachments(attachment))
+	if err != nil {
+		return fmt.Errorf("failed to post message: %w", err)
 	}
 	return nil
 }
